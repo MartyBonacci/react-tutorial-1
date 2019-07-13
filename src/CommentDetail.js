13508178e35cb86a9a237e87039240dd133e9c8d
Marty Bonacci <martybonacci@gmail.com>
2019-07-13 12:33:04 -0600

    pass more info via props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = ({name}) => {
+const CommentDetail = ({avatar, name, comment, timeAgo}) => {
 	return(
 
 			<div className="comment">
 				<a href="/" className="avatar">
-					<img alt="avatar" src={faker.image.avatar()}/>
+					<img alt="avatar" src={avatar}/>
 				</a>
 				<div className="content">
 					<a href="/" className="author">
 						{name}
 					</a>
 					<div className="metadata">
-						<span className="date">Today at 6pm</span>
+						<span className="date">{timeAgo}</span>
 					</div>
-					<div className="text" >Nice blog post</div>
+					<div className="text" >{comment}</div>
 				</div>
 			</div>
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail name='Fred'/>
-			<CommentDetail name='Bob'/>
-			<CommentDetail name='Sue'/>
-			<CommentDetail name='Mary'/>
-			<CommentDetail name='Sam'/>
+
+			<CommentDetail
+				name={faker.name.firstName()}
+				comment={faker.lorem.sentence()}
+				timeAgo='today at 6:00pm'
+				avatar={faker.image.avatar()}
+			/>
+
+			<CommentDetail
+				name='Bob'
+				comment='Favorite comment'
+				timeAgo='yesterday at 3:30pm'
+				avatar={faker.image.avatar()}
+			/>
+
+			<CommentDetail
+				name='Sue'
+				comment='Love this comment'
+				timeAgo='today at 1:00pm'
+				avatar={faker.image.avatar()}
+			/>
+
+			<CommentDetail
+				name='Mary'
+				comment='Awesome comment'
+				timeAgo='yesterday at 2:30pm'
+				avatar={faker.image.avatar()}
+			/>
+
+			<CommentDetail
+				name='Sam'
+				comment='Last comment'
+				timeAgo='today at 6:15pm'
+				avatar={faker.image.avatar()}
+			/>
+
 		</div>
 	);
 };
