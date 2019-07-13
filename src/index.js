e6acbb91c04a68f22be9a46c6086c4d46e02fb90
Marty Bonacci <martybonacci@gmail.com>
2019-07-13 12:51:10 -0600

    wrapped comment detail in approval card by passing comment detail via props

--- src/index.js
+++ src/index.js
@@ -2,45 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import faker from 'faker';
+import ApprovalCard from "./ApprovalCard";
+
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-
+			<ApprovalCard>
 			<CommentDetail
 				name={faker.name.firstName()}
 				comment={faker.lorem.sentence()}
 				timeAgo='today at 6:00pm'
 				avatar={faker.image.avatar()}
 			/>
+			</ApprovalCard>
 
+			<ApprovalCard>
 			<CommentDetail
 				name='Bob'
 				comment='Favorite comment'
 				timeAgo='yesterday at 3:30pm'
 				avatar={faker.image.avatar()}
 			/>
+			</ApprovalCard>
 
+			<ApprovalCard>
 			<CommentDetail
 				name='Sue'
 				comment='Love this comment'
 				timeAgo='today at 1:00pm'
 				avatar={faker.image.avatar()}
 			/>
+			</ApprovalCard>
 
+			<ApprovalCard>
 			<CommentDetail
 				name='Mary'
 				comment='Awesome comment'
 				timeAgo='yesterday at 2:30pm'
 				avatar={faker.image.avatar()}
 			/>
+			</ApprovalCard>
 
+			<ApprovalCard>
 			<CommentDetail
 				name='Sam'
 				comment='Last comment'
 				timeAgo='today at 6:15pm'
 				avatar={faker.image.avatar()}
 			/>
+			</ApprovalCard>
 
 		</div>
 	);
